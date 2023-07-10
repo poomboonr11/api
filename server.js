40d9ddaffe2e3d766bf4c650adad220556a2ccac
poomboonr11 <poom.boonr@mail.kmutt.ac.th>
2023-07-10 14:34:14 +0700

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/api/auth", (req, res) => {
-  res.send("Hello! Node.js");
-});
+app.listen(PORT,() => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
-app.listen(port, () => {
-  console.log("Starting node.js at port " + 3000);
-});
+app.get('/',(req,res) =>{
+    res.send('THis my API RUNNING')
+})
+
+app.get('/about',(req,res) => {
+    res.send('THIS IS ABOUT ROUTE')
+})
+
+module.exports =app;
