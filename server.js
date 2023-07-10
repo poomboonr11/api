9ebe157f1aeef59d61d0dca68f310522913b3a47
poomboonr11 <poom.boonr@mail.kmutt.ac.th>
2023-07-10 15:01:23 +0700

    first commit

--- server.js
+++ server.js
@@ -2,14 +2,37 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,() => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT,() => console.log(`Server running in mode on port ${PORT}`))
 
-app.get('/',(req,res) =>{
-    res.send('THis my API RUNNING')
-})
+app.get('/api/login', async (req, res) => {
+    try {
+        const client = await clientPromise;
+        const db = client.db("account");
+        const { Email, password: pass } = req.body;
+        const userRole ='Unknown';
 
-app.get('/about',(req,res) => {
-    res.send('THIS IS ABOUT ROUTE')
-})
+        const checkUsername = await db.collection("posts").findOne({Email:Email});
+        if (!checkUsername) {
+            const message = "User not found";
+            res.json({ error: message });
+            return; // Exit the function
+        }
+
+        const match = await bcrypt.compare(pass, checkUsername.password);
+        if (match) {
+            const message = { "status": "ok", "user": checkUsername }
+            res.json(message)
+        }
+        else {
+            const message = { "status": "error" }
+            res.json(message)
+        }
+
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'An error occurred while processing the request.' });
+    }
+});
+
 
 module.exports =app;
